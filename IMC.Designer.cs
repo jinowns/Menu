2e6ce277d917fc30bcfa8170128102552f70f7aa
Adriel Silvestrini <adrielsilvestrini@hotmail.com>
2021-03-18 18:03:33 -0300

    Tela de IMC ainda precisa terminar

--- IMC.Designer.cs
+++ IMC.Designer.cs
@@ -29,25 +29,27 @@ namespace Menu
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IMC));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
             this.bntCalcular = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.lbResultado = new System.Windows.Forms.Label();
             this.lbMuitoAbaixo = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.lbAbaixa = new System.Windows.Forms.Label();
+            this.lbNormal = new System.Windows.Forms.Label();
+            this.lbAcimadoPeso = new System.Windows.Forms.Label();
+            this.lbOb1 = new System.Windows.Forms.Label();
+            this.lbOb2 = new System.Windows.Forms.Label();
+            this.lbOb3 = new System.Windows.Forms.Label();
+            this.lbIMC = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -97,26 +99,19 @@ namespace Menu
             this.label5.TabIndex = 4;
             this.label5.Text = "Resultado";
             // 
-            // textBox1
+            // txtPeso
             // 
-            this.textBox1.Location = new System.Drawing.Point(100, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtPeso.Location = new System.Drawing.Point(100, 66);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 5;
             // 
-            // textBox2
+            // txtAltura
             // 
-            this.textBox2.Location = new System.Drawing.Point(100, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(100, 127);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.txtAltura.Location = new System.Drawing.Point(100, 95);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 6;
             // 
             // bntCalcular
             // 
@@ -126,6 +121,7 @@ namespace Menu
             this.bntCalcular.TabIndex = 9;
             this.bntCalcular.Text = "Calcular";
             this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
             // 
             // listBox1
             // 
@@ -156,65 +152,93 @@ namespace Menu
             // lbMuitoAbaixo
             // 
             this.lbMuitoAbaixo.AutoSize = true;
+            this.lbMuitoAbaixo.BackColor = System.Drawing.Color.Yellow;
             this.lbMuitoAbaixo.Location = new System.Drawing.Point(281, 211);
             this.lbMuitoAbaixo.Name = "lbMuitoAbaixo";
             this.lbMuitoAbaixo.Size = new System.Drawing.Size(16, 13);
             this.lbMuitoAbaixo.TabIndex = 12;
             this.lbMuitoAbaixo.Text = "<-";
+            this.lbMuitoAbaixo.Visible = false;
             // 
-            // label7
+            // lbAbaixa
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(281, 224);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(16, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "<-";
+            this.lbAbaixa.AutoSize = true;
+            this.lbAbaixa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lbAbaixa.Location = new System.Drawing.Point(281, 224);
+            this.lbAbaixa.Name = "lbAbaixa";
+            this.lbAbaixa.Size = new System.Drawing.Size(16, 13);
+            this.lbAbaixa.TabIndex = 13;
+            this.lbAbaixa.Text = "<-";
+            this.lbAbaixa.Visible = false;
             // 
-            // label8
+            // lbNormal
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(281, 237);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(16, 13);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "<-";
+            this.lbNormal.AutoSize = true;
+            this.lbNormal.BackColor = System.Drawing.Color.Lime;
+            this.lbNormal.Location = new System.Drawing.Point(281, 237);
+            this.lbNormal.Name = "lbNormal";
+            this.lbNormal.Size = new System.Drawing.Size(16, 13);
+            this.lbNormal.TabIndex = 14;
+            this.lbNormal.Text = "<-";
+            this.lbNormal.Visible = false;
             // 
-            // label9
+            // lbAcimadoPeso
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(281, 250);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(16, 13);
-            this.label9.TabIndex = 15;
-            this.label9.Text = "<-";
+            this.lbAcimadoPeso.AutoSize = true;
+            this.lbAcimadoPeso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbAcimadoPeso.Location = new System.Drawing.Point(281, 250);
+            this.lbAcimadoPeso.Name = "lbAcimadoPeso";
+            this.lbAcimadoPeso.Size = new System.Drawing.Size(16, 13);
+            this.lbAcimadoPeso.TabIndex = 15;
+            this.lbAcimadoPeso.Text = "<-";
+            this.lbAcimadoPeso.Visible = false;
             // 
-            // label10
+            // lbOb1
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(281, 263);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(16, 13);
-            this.label10.TabIndex = 16;
-            this.label10.Text = "<-";
+            this.lbOb1.AutoSize = true;
+            this.lbOb1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbOb1.Location = new System.Drawing.Point(281, 263);
+            this.lbOb1.Name = "lbOb1";
+            this.lbOb1.Size = new System.Drawing.Size(16, 13);
+            this.lbOb1.TabIndex = 16;
+            this.lbOb1.Text = "<-";
+            this.lbOb1.Visible = false;
             // 
-            // label11
+            // lbOb2
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(281, 276);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(16, 13);
-            this.label11.TabIndex = 17;
-            this.label11.Text = "<-";
+            this.lbOb2.AutoSize = true;
+            this.lbOb2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lbOb2.Location = new System.Drawing.Point(281, 276);
+            this.lbOb2.Name = "lbOb2";
+            this.lbOb2.Size = new System.Drawing.Size(16, 13);
+            this.lbOb2.TabIndex = 17;
+            this.lbOb2.Text = "<-";
+            this.lbOb2.Visible = false;
             // 
-            // label12
+            // lbOb3
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(281, 289);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(16, 13);
-            this.label12.TabIndex = 18;
-            this.label12.Text = "<-";
+            this.lbOb3.AutoSize = true;
+            this.lbOb3.BackColor = System.Drawing.Color.Red;
+            this.lbOb3.Location = new System.Drawing.Point(281, 289);
+            this.lbOb3.Name = "lbOb3";
+            this.lbOb3.Size = new System.Drawing.Size(16, 13);
+            this.lbOb3.TabIndex = 18;
+            this.lbOb3.Text = "<-";
+            this.lbOb3.Visible = false;
+            // 
+            // lbIMC
+            // 
+            this.lbIMC.AutoSize = true;
+            this.lbIMC.Location = new System.Drawing.Point(130, 130);
+            this.lbIMC.Name = "lbIMC";
+            this.lbIMC.Size = new System.Drawing.Size(16, 13);
+            this.lbIMC.TabIndex = 19;
+            this.lbIMC.Text = "...";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // IMC
             // 
@@ -222,19 +246,19 @@ namespace Menu
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(337, 450);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbIMC);
+            this.Controls.Add(this.lbOb3);
+            this.Controls.Add(this.lbOb2);
+            this.Controls.Add(this.lbOb1);
+            this.Controls.Add(this.lbAcimadoPeso);
+            this.Controls.Add(this.lbNormal);
+            this.Controls.Add(this.lbAbaixa);
             this.Controls.Add(this.lbMuitoAbaixo);
             this.Controls.Add(this.lbResultado);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.bntCalcular);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -255,18 +279,19 @@ namespace Menu
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
         private System.Windows.Forms.Button bntCalcular;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label lbResultado;
         private System.Windows.Forms.Label lbMuitoAbaixo;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lbAbaixa;
+        private System.Windows.Forms.Label lbNormal;
+        private System.Windows.Forms.Label lbAcimadoPeso;
+        private System.Windows.Forms.Label lbOb1;
+        private System.Windows.Forms.Label lbOb2;
+        private System.Windows.Forms.Label lbOb3;
+        private System.Windows.Forms.Label lbIMC;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- IMC.cs
+++ IMC.cs
@@ -12,14 +12,105 @@ namespace Menu
 {
     public partial class IMC : Form
     {
+        private double peso = 0;
+        private double altura = 0;
+        private double resultado = 0;
+        private int alxiliar = 0;
         public IMC()
         {
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void bntCalcular_Click(object sender, EventArgs e)
         {
+            lbMuitoAbaixo.Visible = false;
+            lbAbaixa.Visible = false;
+            lbNormal.Visible = false;
+            lbAcimadoPeso.Visible = false;
+            lbOb1.Visible = false;
+            lbOb2.Visible = false;
+            lbOb3.Visible = false;
+            alxiliar = 1;
 
+            peso = double.Parse(txtPeso.Text);
+            altura = double.Parse(txtAltura.Text);
+
+            resultado = peso / Math.Pow(altura,2);
+            resultado = Math.Round(resultado,2);
+
+            lbIMC.Text = resultado.ToString();
+            if(resultado < 17)
+            {
+                
+            }
+            else
+            if(resultado >= 17 && resultado <= 18.49)
+            {
+                timer1.Enabled = true;            
+            }
+            else
+            if(resultado >= 18.5 && resultado <= 24.99)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            if(resultado >= 25 && resultado <= 29.99)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            if(resultado >= 30 && resultado <= 34.99)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            if (resultado >= 35 && resultado <= 39.99)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            if(resultado > 40)
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (resultado < 17)
+            {
+                lbMuitoAbaixo.Visible = true;
+            }
+            else
+            if (resultado >= 17 && resultado <= 18.49)
+            {
+                lbAbaixa.Visible = true;
+            }
+            else
+            if (resultado >= 18.5 && resultado <= 24.99)
+            {
+                lbNormal.Visible = true;
+            }
+            else
+            if (resultado >= 25 && resultado <= 29.99)
+            {
+                lbAcimadoPeso.Visible = true;
+            }
+            else
+            if (resultado >= 30 && resultado <= 34.99)
+            {
+                lbOb1.Visible = true;
+            }
+            else
+            if (resultado >= 35 && resultado <= 39.99)
+            {
+                lbOb2.Visible = true;
+            }
+            else
+            if (resultado > 40)
+            {
+                lbOb3.Visible = true;
+            }
         }
     }
 }
